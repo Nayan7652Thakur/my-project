112627c641a81ddc4f4562007db8d84e320e0265
Nayan7652Thakur <nayanthakur7652@gmail.com>
2024-10-12 13:56:08 +0530

    error

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,23 @@
+import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
+
+export const signup = async (req, res , next) => {
+
+    const { username, email, password } = req.body;
+
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+
+    const newUser = new User({ username, email, password: hashedPassword })
+
+    try {
+
+        await newUser.save()
+        res.status(201).json('signup sucess')
+
+    } catch (error) {
+       next(error)
+    }
+
+
+}
